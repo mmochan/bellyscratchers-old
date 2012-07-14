7db7961bff20c83f053895ff97125bf3cf6494e1
Mike Mochan <mmochan@mac.com>
2012-07-14 12:17:23 +1000

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Backscratchers::Application.config.secret_token = 'e7ed0a9f2c03c5ca761f1040d79566d1e4befd983eb51cf99feb506397dab941540b5dddd677daf8fdc100f33284e62d34b760b51bb24893d2e16ccc6b3cbf36'
