a87c9224df6ad4b9c6dbe965013d502abdce0330
mmochan <mmochan@mac.com>
2012-10-28 14:27:39 +1000

    fixed spelling

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -51,7 +51,7 @@ Bellyscratchers::Application.configure do
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
   
-  onfig.assets.initialize_on_precompile = false
+  config.assets.initialize_on_precompile = false
 
   # Enable threaded mode
   # config.threadsafe!
