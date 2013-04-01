0f5bbfa5db44ba38eecff11646802fb67738e415
mmochan <mmochan@mac.com>
2013-04-01 18:18:28 +1000

    removed coffescript from bookings.js.coffee, which appeared to be ignoring date input

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -2,4 +2,7 @@ class Booking < ActiveRecord::Base
 	belongs_to :customer
   attr_accessible :dropoff_date,  :pickup_date, :dog_count, :pickup_required
   validates :dog_count, :numericality => true
+  validates :dropoff_date, :presence => true
+  validates :pickup_date, :presence => true
+
 end
