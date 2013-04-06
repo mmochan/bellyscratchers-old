f1cde810ecb3aac1809b97d173b8864578e77ac1
mmochan <mmochan@mac.com>
2013-04-06 17:22:59 +1000

    Customer validation to ensure at least one dog is booked in

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -6,13 +6,18 @@ class Booking < ActiveRecord::Base
   validates :dropoff_date, :presence => true
   validates :pickup_date, :presence => true
 
-  validate :dropoff_is_before_pickup, :back_in_time, :at_least_one_day_notice, :too_long
+  validate :dropoff_is_before_pickup, :back_in_time, :one_day_notice, :too_long
+  validate :dogs
 
   def dropoff_is_before_pickup
   	errors.add(:dropoff_date, "Can't drop em off before we pick em up") unless self.dropoff_date < self.pickup_date	
   end
 
-  def at_least_one_day_notice
+  def back_in_time
+  	errors.add(:dropoff_date, "I can't travel back in time - my time machine is broken") unless self.dropoff_date >= Date.today
+  end
+
+  def one_day_notice
   	errors.add(:dropoff_date, "That's a bit short notice for me") unless self.dropoff_date > Date.today + 1
   end
 
@@ -20,7 +25,8 @@ class Booking < ActiveRecord::Base
   	errors.add(:pickup_date, "That's a very long stay, are you sure about those dates?") unless (self.pickup_date - self.dropoff_date).to_i < 20
   end
 
-  def back_in_time
-  	errors.add(:dropoff_date, "I can't travel back in time - my time machine is broken") unless self.dropoff_date >= Date.today
+  def dogs
+  	errors.add(:visitors, "Surely one of them is coming?") if self.visitors[0].empty?
+  	
   end
 end
